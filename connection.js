200377f2da37622f0887872386bbae3537b32262
maheskkarande1505 <karandemahesh4@gmail.com>
2025-01-14 11:53:23 +0530

    password change

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ var util = require("util");
 var conn = mysql.createConnection({
     "host":"b8n1rh8fy2ujm9fobkqs-mysql.services.clever-cloud.com",
     "user":"us7k4g1tl71cmwlb",
-    "password":"us7k4g1tl71cmwlb",
+    "password":"yNySfdZedmNRpaNbXaX5",
     "database":"b8n1rh8fy2ujm9fobkqs"
 });
 
